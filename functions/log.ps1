be8d0c25275b34659ee582e28ec4dc0066acde6d
jakub.pawlowski <jakub.pawlowski@legimi.com>
2016-02-02 10:33:27 +0100

    fix logging module

--- functions/log.ps1
+++ functions/log.ps1
@@ -1,9 +1,11 @@
 
 $asm = [Reflection.Assembly]::LoadFile("$PSScriptRoot\..\lib\Crayons.dll")
 
-$p = [Crayons.Patterns.Pattern]::new(
-    "(?<magenta>'.*')"
-)
+$p = new-object -type Crayons.Patterns.Pattern
+$p.Add("(?<magenta>'.*')", "colorize quoted names")
+
+
+[Crayons.CrayonString]::EscapeChar = '`'
 
 [Crayons.Crayon]::Configure({
     param($text, $color) 
@@ -33,9 +35,3 @@ function Log-Progress($activity, $status, $percentComplete, $id) {
     Write-Progress @PSBoundParameters 
     write-host $activity : $status
 }
-
-Log-Info "testing 'this'"
-Log-Info "testing 'that'"
-Log-progress -activity "testing this module" -status "running test 1" -percentComplete 59
-#write-progress -activity "testing this module" -status "running test 1" -percentComplete 59
-Start-Sleep -Seconds 3
